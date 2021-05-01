af055040d62949494e51a64ecb69b1b24369d664
Cheddadi mohamed-yassine <46085172+YassineChe@users.noreply.github.com>
2021-05-01 00:00:55 +0000

    removing conversation stuff

--- database/migrations/2021_04_30_234714_create_chatflows_table.php
+++ database/migrations/2021_04_30_234714_create_chatflows_table.php
@@ -15,9 +15,11 @@ class CreateChatflowsTable extends Migration
     {
         Schema::create('chatflows', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('conversation_id')->references('id')->on('conversations');
-            $table->enum('from', ['admin', 'restaurant', 'derlivery']);
-            $table->enum('to', ['admin', 'restaurant', 'derlivery']);
+            // $table->foreignId('conversation_id')->references('id')->on('conversations');
+            $table->foreignId('admin_id')->nullable()->references('id')->on('admins');
+            $table->foreignId('delivery_id')->nullable()->references('id')->on('deliveries');
+            $table->enum('from', ['admin', 'derlivery']);
+            $table->enum('to', ['admin', 'derlivery']);
             $table->text('message');
             $table->timestamp('seen_at');
             $table->timestamps();
