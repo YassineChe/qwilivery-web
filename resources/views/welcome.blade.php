a570542b2ea6934e0ccdc3f72e851a56279a6e80
Cheddadi mohamed-yassine <46085172+YassineChe@users.noreply.github.com>
2021-05-05 22:20:59 +0000

    adding favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href=" {{ asset('/css/custom.css') }}">
+    <link rel="icon" href="/images/logo-wt.svg">
+    <link rel="mask-icon" href="/images/logo-wt.svg">
     <title>Qwelivery | Plate-forme</title>
 </head>
 
