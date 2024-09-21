61505a01b0a6ff41a3325c1da230657184de097b
ycheddadi <ycheddadi@manaosconsulting.com>
2024-09-21 17:58:49 +0100

    new file

--- config/larafirebase.php
+++ config/larafirebase.php
@@ -2,5 +2,5 @@
 
 return [
     'project_id' => 'qwilivery-7eb0f',
-    'firebase_credentials' => public_path('firebase/qwilivery-7eb0f-firebase-adminsdk-an7mf-0c221a7889.json'),
+    'firebase_credentials' => public_path('firebase/qwilivery-7eb0f-firebase-adminsdk-an7mf-c41751004a.json'),
 ];
